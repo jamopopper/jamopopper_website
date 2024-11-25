45a33ee7c174e74f4507938971e8d075af4386aa
jamopopper <jace_palmer@live.com>
2024-11-25 14:51:28 -0800

    he lookin good af rn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var yapper = document.getElementById("yapping_man");
 var yap_text = document.getElementsByClassName("speech_bubble")[0];
-var yap_box = yap_text.childNodes[0];
+var yap_box = yap_text.childNodes[1];
 var a = Math.floor(Math.random()*750 + 750);
 
 move_yapper = function() {
@@ -16,25 +16,29 @@ move_yapper = function() {
     }
 }
 
-talking = function() {
-    console.log("starting talking");
+
+talking_start = function() {
     yapper.src = "images/yapping.png";
     yap_text.style.display = "block";
-    yap_box.textContent = "Hello!! Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Nisi, Quo Quisquam! Labore, Quae Quia?";
-    console.log("talking on");
+    yap_box.textContent = "Hello!";
     setTimeout(function() {
-        console.log("stopping talking");
-        yapper.src = "images/silent.png";
-        yap_text.style.display = "none";
-        yap_box.textContent = "";
-        console.log("talking off");
-    }, 6000);
+        yap_box.textContent = "Click or tap on the icons below to see what I'm up to!";
+        setTimeout(talking_end, 4000);
+    }, 2000);
+};
+
+talking_end = function() {
+    console.log("stopping talking");
+    yapper.src = "images/silent.png";
+    yap_text.style.display = "none";
+    yap_box.textContent = "";
+    console.log("talking off");
     a = Math.floor(Math.random()*10000 + 10000);
-    setTimeout(talking,a);
+    setTimeout(talking_start,a);
     console.log("queue created");
 };
 
 move_yapper();
-setTimeout(talking,a)
+setTimeout(talking_start,a)
 
 window.addEventListener("resize", move_yapper)
