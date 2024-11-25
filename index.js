7963c02a4ef20a0f946bcad5c9e03f87ea0500b2
jamopopper <jace_palmer@live.com>
2024-11-25 00:14:42 -0800

    he shmoves and stuff

---
+++ index.js
@@ -0,0 +1,25 @@
+var yapper = document.getElementById("yapping_man");
+var a = Math.floor(Math.random()*750 + 750);
+
+notify = function() {
+    if (window.innerHeight / window.innerWidth > 4/3) {
+        yapper.style.bottom = "20vh";
+    } else {
+        yapper.style.bottom = "calc(20vh + " + 
+        (((window.innerHeight / window.innerWidth) - 4/3) * 5).toString() + "vw)";
+    }
+}
+
+print = function() {
+    yapper.src = "images/yapping.png";
+    setTimeout(function() {
+        yapper.src = "images/silent.png";
+    }, 3500);
+    a = Math.floor(Math.random()*10000 + 10000);
+    setTimeout(print,a);
+};
+
+notify();
+setTimeout(print,a)
+
+window.addEventListener("resize", notify)
